15b74901467dcff99de6ce3e8dc651da1f65905b
Jovanny Cruz <jovannypcg@yahoo.com>
2018-04-16 13:00:11 -0500

    First commit

---
+++ src/test/java/com/figureeight/cclient/CclientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.figureeight.cclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CclientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
